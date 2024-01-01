f1669fdd5da2982596fe8cb7e2f342241ca91aa8
Carlos González <carlos.rgn@gmail.com>
2024-01-01 16:22:54 -0600

    the issue

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,21 @@
-import type { MetaFunction } from "@remix-run/node";
+import { useLoaderData } from '@remix-run/react';
+import { z } from 'zod';
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+export function loader() {
+  const envs = z
+    .object({
+      apiKey: z.string(),
+    })
+    .parse({
+      apiKey: undefined, // this will throw an error
+    });
+
+  return {
+    envs,
+  };
+}
 
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+  const data = useLoaderData<typeof loader>();
+  return <div>{JSON.stringify(data, null, 2)}</div>;
 }
